e4ac7b1f2f22654bc391d8de4979f03a5012c62f
spac3sc <darealeljantmg6@gmail.com>
2025-01-02 17:27:07 +0800

    Added remove button to added choices

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const submitButton = document.getElementById('subnt');
 const pick_random = document.getElementById("pick_random");
-let count = 1;
+//let count = 1;
 
 const min = 1;
 let max = 1;
 let random;
-let added_count;
+let added_count = 1;
 
 submitButton.onclick = function(){
     if (document.getElementById('choice').value == ""){
@@ -21,17 +21,29 @@ submitButton.onclick = function(){
 
     let entered_choices = document.getElementById('entered_choices');
     entered_choices.appendChild(new_choice_p);
-    added_count = count++;
+    added_count = added_count++ + 1;
     new_choice_p.id = "choice" + added_count;
     new_choice_p.className = "choices";
     console.log(new_choice_p.id);
     document.getElementById('choice').value = "";
 
+    let remove_button = document.createElement('button');
+    remove_button.textContent = "X";
+    remove_button.id = "add" + added_count;
+
+    remove_button.onclick = function(){
+        added_count= added_count - 1;
+        document.getElementById(new_choice_p.id).remove();
+        console.log("remove: " + added_count);
+    }
+    new_choice_p.appendChild(remove_button);
+    console.log(remove_button.id);
     }
 }
 
 pick_random.onclick = function(){
     let max = added_count;
+    console.log("randomizing")
     let random = Math.floor(Math.random() * max) + min;
         alert(document.getElementById("choice" + random).textContent);
     }
